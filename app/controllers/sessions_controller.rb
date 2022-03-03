bebc5f21960a76b160969da9ec22e0a4d4f48d87
yongjia7 <97008811+yongjia7@users.noreply.github.com>
2022-03-02 19:01:13 -0600

    Initial commit

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  
+  def create
+    @user = User.where({ email: params["email"] })[0]
+    if @user
+      if BCrypt::Password.new(@user.password) == params["password"]
+        session[:user_id] = @user.id
+        flash[:notice] = "Welcome."
+        redirect_to "/posts"
+      else
+        flash[:notice] = "Nope."
+        redirect_to "/sessions/new"
+      end
+    else
+      flash[:notice] = "Nope."
+      redirect_to "/sessions/new"
+    end
+  end
+end
